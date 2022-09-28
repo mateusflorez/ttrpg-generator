7197b677c8827a773355e795a401515d38cd99d1
Mateus Paz <54851249+mateusflorez@users.noreply.github.com>
2022-09-28 10:51:39 -0300

    index + options

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'static_pages/index'
+  root 'static_pages#index'
+
+  get 'options/1'
+
 end
