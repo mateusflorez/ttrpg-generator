7d5cd025fe43050ca743f145cb88c338a55e0e02
Mateus Paz <54851249+mateusflorez@users.noreply.github.com>
2022-09-29 15:21:33 -0300

    travel - climate

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -14,4 +14,11 @@ class OptionsController < ApplicationController
     end
   end
 
+  def travel
+    @resp = params[:param1]
+    respond_to do |format|
+      format.js
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
 
   get 'options/answer'
   get 'options/dices'
+  get 'options/travel'
 
 end
